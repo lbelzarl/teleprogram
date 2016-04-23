d3eab4e04c22d8e0fc174592fc925a82f401a3e7
Belzar <belzar@yandex.ru>
2016-04-23 23:46:01 +0300

    Переключения программ по дням недели

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+document.querySelector('.filter-days').addEventListener('click', function(e) {
+    if (e.target.className == 'nkjh') {
+        var hideShow = document.querySelectorAll('.hide-show'),
+            show = '.' + e.target.getAttribute('data-day');
+
+        for (var i = 0; i < hideShow.length; i++) {
+            hideShow[i].style.display = 'none';
+        }
+
+        document.querySelector(show).style.display = 'block';
+    }
+});
